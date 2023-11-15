080c71a70c368612e44189324ecc8d6df9a3461c
dtloveless <dtloveless@penguin>
2023-11-15 11:17:41 -0700

    Finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        string userChoice;
+        
+        do
+        {
+            int magicNumber = randomGenerator.Next(1, 101);
+            // Console.WriteLine(magicNumber);
+            string response;
+            int guess;
+            int numGuesses = 0;
+            do
+            {
+                Console.Write("What is your guess? ");
+                response = Console.ReadLine();
+                guess = int.Parse(response);
+                numGuesses += 1;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+            } while (guess != magicNumber);
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"You got it in {numGuesses} guesses!");
+            Console.Write("Do you want to play again ('yes'/'no')? ");
+            userChoice = Console.ReadLine();
+
+        } while (userChoice == "yes");
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
