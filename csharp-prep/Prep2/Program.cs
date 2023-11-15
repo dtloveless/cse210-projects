be64164f04f4474b425e02878bd5c67e813f155a
dtloveless <dtloveless@penguin>
2023-11-14 22:43:45 -0700

    Completed Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade point earned for the class: ");
+        string userInput = Console.ReadLine();
+        int gradePoint = int.Parse(userInput);
+
+        string letter;
+        bool isPosSign = true;
+        bool isNegSign = true;
+        if (gradePoint >= 90)
+        {
+            letter = "A";
+            isPosSign = false;
+        }
+        else if (gradePoint >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePoint >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePoint >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+            isPosSign = false;
+            isNegSign = false;
+        }
+
+        int remainder = gradePoint % 10;
+        if (isPosSign && remainder >= 7)
+        {
+            letter += "+";
+        }
+        else if (isNegSign && remainder < 3 && gradePoint != 100)
+        {
+            letter += "-";
+        }
+
+        Console.WriteLine($"Letter earned: {letter}");
+        if (gradePoint >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
